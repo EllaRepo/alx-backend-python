425f356ac90cb4a3a521f809f79b42778be0131f
Eliab E <eliaberango@gmail.com>
2023-12-07 05:41:17 -0800

    Add 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module that defines a function that returns a floor of float number
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Type-annotated function returns a floor of float number
+    Args:
+        n(float): a number
+    """
+    return math.floor(n)
