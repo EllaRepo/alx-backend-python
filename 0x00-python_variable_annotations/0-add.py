58f87c8132ab20770db8fb25538a50372f4c910e
Eliab E <eliaberango@gmail.com>
2023-12-07 05:29:37 -0800

    Add 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module that defines a function that adds two float number
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Type-annotated function that adds two numbers
+    Args:
+        a(float): a number
+        b(float): b number
+    """
+    return a + b
