e77ab02bd1776267cc31975ce2ccc74e72c6e1db
Eliab E <eliaberango@gmail.com>
2023-12-07 05:58:06 -0800

    Add sheband

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,10 +1,14 @@
-"""Module that defines a function that returns string representation of number
+#!/usr/bin/env python3
+"""Module that defines a function that returns string
+   representation of number
 """
 
 
 def to_str(n: float) -> str:
-    """Type-annotated function returns string representation of number
+    """Returns a string representation of a float
     Args:
-        n(float): a number
+        n: A float number to be converted to a string
+    Returns:
+        A string representation of the float
     """
     return str(n)
