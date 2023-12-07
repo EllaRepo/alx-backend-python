b15c1ca6b6ef4d958e9cce7dbef567af2415a57c
Eliab E <eliaberango@gmail.com>
2023-12-07 05:44:38 -0800

    Add 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+"""Module that defines a function that returns string representation of number
+"""
+import math
+
+
+def to_str(n: float) -> str:
+    """Type-annotated function returns string representation of number
+    Args:
+        n(float): a number
+    """
+    return str(n)
