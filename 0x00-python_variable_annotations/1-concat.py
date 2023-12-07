0bd0ffa798beae003dc2d0a7fad8d15595c80e10
Eliab E <eliaberango@gmail.com>
2023-12-07 05:36:12 -0800

    Add 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module that defines a function that concatenates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Type-annotated function that concatenates two strings
+    Args:
+        str1(str): string
+        str2(str): string
+    """
+    return str1 + str2
