7000d8487800754b60c6ffe9d8ab536b80218c32
Eliab E <eliaberango@gmail.com>
2023-12-11 01:56:42 -0800

    Make type-annotated

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,8 +3,7 @@
    with the specified max_delay
 """
 import asyncio
-from typing import List
-
+from typing import List, Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -16,13 +15,13 @@ async def wait_n(n: int, max_delay: int) -> List:
     Returns:
         The list of all the delays (float values)
     """
-    delays = []
+    delays: List[float] = []
 
     async def wait_and_append(delay):
         result = await wait_random(delay)
         delays.append(result)
 
-    tasks = [wait_and_append(max_delay) for _ in range(n)]
+    tasks: List[Any] = [wait_and_append(max_delay) for _ in range(n)]
     await asyncio.gather(*tasks)
 
     return delays
