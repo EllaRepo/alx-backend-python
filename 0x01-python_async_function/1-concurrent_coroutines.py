3b56698c1b023c9f3954d8972af3af430ff011ac
Eliab E <eliaberango@gmail.com>
2023-12-11 01:58:13 -0800

    Make type-annotated

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List, Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawn wait_random n times with the specified max_delay
     Args:
         n(int): number of times function spawned
