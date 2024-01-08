a14a91c2be0c523b878c86083db622f270859896
Eliab E <eliaberango@gmail.com>
2024-01-08 13:03:51 -0800

    Add test_public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Unit test module for utils module.
+"""Unit test module for client module.
 """
 import unittest
 from typing import Dict
@@ -36,3 +36,18 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked_fn.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
+
+    def test_public_repos_url(self) -> None:
+        """Tests the public_repos_url` property.
+        """
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+                ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
