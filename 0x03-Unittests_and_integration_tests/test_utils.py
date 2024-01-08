9a9b53aa18826aca8e3f39b853358d4d0a8b0827
Eliab E <eliaberango@gmail.com>
2024-01-08 12:24:04 -0800

    Add test_access_nested_map_except

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,3 +24,18 @@ class TestAccessNestedMap(unittest.TestCase):
                                      nested dictionary
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence) -> None:
+        """Test the access_nested_map method raises an error when expected to
+        Args:
+            nested_map (Dict): A dictionary that may have nested dictionaries
+            path (List, tuple, set): Keys to get to the required value in the
+                                     nested dictionary
+        """
+        with self.assertRaises(Exception):
+            access_nested_map(nested_map, path)
